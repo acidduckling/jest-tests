b54efcd6f9b558560195627d449627a3cb091514
Your Name <acidduckling@gmail.com>
2018-07-01 20:03:03 +1000

    Basic mocks with Jest

--- script2.test.js
+++ script2.test.js
@@ -19,4 +19,22 @@ it('Calls SWAPI to get people with a promise', () => {
   });
 });
 
+it('getPeople returns count asnd results', () => {
+  expect.assertions(4);
 
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 87,
+          results: [0, 1, 2, 3, 4, 5]
+        })
+    })
+  );
+  return swapi.getPeoplePromise(mockFetch).then(data => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith('https://swapi.co/api/people');
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
